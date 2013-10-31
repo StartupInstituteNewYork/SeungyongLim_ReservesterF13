4b47d89841c42c15396f332d2973bbdc6883cb5f
Seung Lim <slopeofhope81@gmail.com>
2013-10-31 19:13:59 -0400

    updated database.yml

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '460bbc9489d00ba715dba968c7b88fce454717693a77737ed3cbc8b62f3dd8a79cbc888a2e968fc94ca693016e945b23033ab9a8afdc826759268091fb024e8d'
