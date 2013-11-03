907f0b794c58eafe4469f420859ae3d0eded3e28
Seung Lim <slopeofhope81@gmail.com>
2013-11-03 12:27:04 -0500

    Modified database.yml

---
+++ reservester/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reservester::Application.config.secret_key_base = '909b67aa03c9139c41bc58779752b84c93a09a5888c63ebaf2b72f511a05c431e197bf85fc6a3702f7f49257fb48c675183f6a817b18cb306eb0f423cb1a75f8'
