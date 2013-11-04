0cff2f0ffd335fac47c2984ae81433f04f756275
Seung Lim <slopeofhope81@gmail.com>
2013-11-04 16:04:09 -0500

    Made CRUD

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,16 +1,39 @@
 class RestaurantsController < ApplicationController
+  
   def index
+    @restaurants=Restaurant.all
   end
+  
   def new
+    @restaurant=Restaurant.new
   end
+  
   def create
   	@restaurant = Restaurant.new(post_params)
   	@restaurant.save
 
   	redirect_to @restaurant	
   end
+  
   def show
   	@restaurant=Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant=Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant=Restaurant.find(params[:id])
+    @restaurant.update(post_params)
+    redirect_to @restaurant
+  end
+
+  def destroy
+    @restaurant=Restaurant.find(params[:id])
+    @restaurant.destroy
+
+    redirect_to restaurants_path
   end
 
 private
