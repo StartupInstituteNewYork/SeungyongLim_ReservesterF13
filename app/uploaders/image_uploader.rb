023fd4b6c2841c23122afa6bc28ee7602ec55187
Seung Lim <slopeofhope81@gmail.com>
2013-11-14 05:37:14 -0500

    imge uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,10 +3,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
     storage :fog
-
-    def store_dir
-      'reservester-for-startup/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}'
-    end
+    
+    
     
     version :masthead do
        process :resize_to_fit => [600, 500]
